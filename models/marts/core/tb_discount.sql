6c3778f615ec26757728ed60a4e344e989fdd599
jessica.carvalho <jessica.carvalho@blueshift.com.br>
2023-06-16 19:45:43 -0300

    teste

--- models/marts/core/tb_discount.sql
+++
@@ -1,6 +0,0 @@
-select 
-    _ROW as id,
-    order_value as vl_order,
-    discount as vl_discount
-from 
-    {{ ref('stg_discounts')}}
--- tests/assert_positive_value_for_value_discounts.sql
+++
@@ -1,8 +0,0 @@
--- Refunds have a negative amount, so the total amount should always be >= 0.
--- Therefore return records where this isn't true to make the test fail.
-select
-  current_date() as dt,
-	sum(order_value) as order_value_amount
-from {{ ref('stg_discounts') }}
-group by 1
-having not(order_value_amount >= 0)
