422fdcff150375507b8e5865b02cc16e87a1ad16
jessica.carvalho <jessica.carvalho@blueshift.com.br>
2023-06-16 21:00:13 -0300

    teste

--- models/trusted/google_sheets/tb_discounts.sql
+++ models/trusted/google_sheets/tb_discounts.sql
@@ -1,5 +1,5 @@
 select
     _ROW as id,
     order_value as vl_order,
-    discount as vl_discount
+    cast(discount as decimal(1,2)) as vl_discount
 from {{ source('google_sheets', 'googlesheets_discounts') }}
